b80bafc557921e75f637fd0b3a77cdd0d0f2f5d7
MerlierR <robbert.merlier@axians.com>
2017-12-29 23:46:47 +0100

    Add script execution time, cleanup setup

--- src/day01/index.js
+++ src/day01/index.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { manhattanDistance } from '../Position';
 import { followInstructions, parseInput } from './day01';
 
+const start = process.hrtime();
 (async () => {
     const file = path.join(__dirname, 'day01.txt');
     const data = fs.readFileSync(file, 'utf8');
@@ -18,5 +19,7 @@ import { followInstructions, parseInput } from './day01';
     const dupeDistance = Math.min(...[...dupes].map((dupePosition) => manhattanDistance(dupePosition)));
     console.log(`Part 2: ${dupeDistance}`);
 
+    const end = process.hrtime(start);
+    console.info('Process exited in %dms', (end[0] * 1000 + end[1] / 1000000).toPrecision(3));
     process.exit();
 })();

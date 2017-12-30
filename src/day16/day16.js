e1a3df2e39d84bd06eba695acd043478ed3646a0
MerlierR <robbert.merlier@axians.com>
2017-12-30 19:39:27 +0100

    Day 16 - array solution (speed x2)

--- src/day16/day16.js
+++ src/day16/day16.js
@@ -1,5 +1,5 @@
 export function checksum(input: string, length: number): string {
-    let result = input;
+    let result = input.split('').map((char) => parseInt(char, 10));
 
     // expand
     while (result.length <= length) {
@@ -14,30 +14,24 @@ export function checksum(input: string, length: number): string {
         result = partialChecksum(result);
     }
 
-    return result;
+    return result.join('');
 }
 
-export function trimInput(input: string, length: number): string {
-    return input.substr(0, length);
+export function trimInput(input: number[], length: number): number[] {
+    return input.slice(0, length);
 }
 
-export function expandInput(input: string): string {
-    const b = input.split('')
-        .map((char) => (parseInt(char, 10) + 1) % 2)
-        .reverse()
-        .join('');
-
-    return `${input}0${b}`;
+export function expandInput(input: number[]): number[] {
+    const b = input.map((char) => (char + 1) % 2).reverse();
+    return [...input, 0, ...b];
 }
 
-export function partialChecksum(input: string): string {
-    const splitInput = input.split('');
-
+export function partialChecksum(input: number[]): number[] {
     const l = input.length;
-    let result = '';
+    const result = [];
     for (let i = 0; i < l; i += 2) {
-        if (splitInput[i] === splitInput[i + 1]) result += '1';
-        else result += '0';
+        if (input[i] === input[i + 1]) result.push(1);
+        else result.push(0);
     }
 
     return result;
--- src/day16/day16.test.js
+++ src/day16/day16.test.js
@@ -3,28 +3,31 @@ import { checksum, expandInput, partialChecksum, trimInput } from './day16';
 describe('Day 16: Dragon Checksum', () => {
 
     it('can expand the input for "1"', () => {
-        expect(expandInput('1')).toBe('100');
+        expect(expandInput([1])).toEqual([1, 0, 0]);
     });
 
     it('can expand the input for "0"', () => {
-        expect(expandInput('0')).toBe('001');
+        expect(expandInput([0])).toEqual([0, 0, 1]);
     });
 
     it('can expand the input for "11111"', () => {
-        expect(expandInput('11111')).toBe('11111000000');
+        expect(expandInput([1, 1, 1, 1, 1])).toEqual([1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]);
     });
 
     it('can expand the input for "111100001010"', () => {
-        expect(expandInput('111100001010')).toBe('1111000010100101011110000');
+        expect(expandInput([1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0]))
+            .toEqual([1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0]);
+        expect(expandInput([1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0]))
+            .toEqual([1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0]);
     });
 
     it('can trim inputs', () => {
-        expect(trimInput('0123456789', 5)).toBe('01234');
+        expect(trimInput([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 5)).toEqual([0, 1, 2, 3, 4]);
     });
 
     it('can calculate partial checksums', () => {
-        expect(partialChecksum('110010110100')).toBe('110101');
-        expect(partialChecksum('110101')).toBe('100');
+        expect(partialChecksum([1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0])).toEqual([1, 1, 0, 1, 0, 1]);
+        expect(partialChecksum([1, 1, 0, 1, 0, 1])).toEqual([1, 0, 0]);
     });
 
     it('can calculate the checksum', () => {
